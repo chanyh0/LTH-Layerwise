4a238aa86478ed46d163f34f86b2e5d32b65d346
Ch4nYH <hfercxx@hotmail.com>
2021-03-03 21:12:26 +0800

    p

--- pruning_utils_2.py
+++ pruning_utils_2.py
@@ -354,7 +354,7 @@ def prune_model_custom_one_random(model, mask_dict, random_index = -1):
             print('pruning layer with custom mask:', name)
             prune.CustomFromMask.apply(m, 'weight', mask=mask_dict[name+'.weight_mask'])
             if index == random_index:
-                prune.RandomUnstructured.apply(m, 'weight', amount=(mask_dict[name+'.weight_mask']==0).sum().float() / mask_dict[name+'.weight_mask'].numel())
+                prune.RandomUnstructured.apply(m, 'weight', amount=(mask_dict[name+'.weight_mask']==0).sum().int())
             index += 1
 
 
