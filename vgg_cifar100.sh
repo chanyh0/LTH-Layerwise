b6340a09afb7b86d0b0372797470d01500038e56
Ch4nYH <hfercxx@hotmail.com>
2021-03-03 21:38:36 +0800

    p

--- vgg_cifar100.sh
+++ vgg_cifar100.sh
@@ -1,4 +1,4 @@
 for i in $(seq 0 16);
 do
-CUDA_VISIBLE_DEVICES=4 python -u main_eval_one_random.py --data datasets --dataset cifar100 --arch vgg16_bn --save_dir vgg16_bn_cifar10_baseline --pretrained LT-model-coreset/cifar10_vgg16_LT/corner_71.87.pth.tar --mask_dir LT-model-coreset/cifar10_vgg16_LT/corner_71.87.pth.tar --seed 1 --conv1 --lr 0.1 --fc --random-index ${i} > vgg_cifar100_${i}.out
+CUDA_VISIBLE_DEVICES=4 python -u main_eval_one_random.py --data datasets --dataset cifar100 --arch vgg16_bn --save_dir vgg16_bn_cifar100_baseline --pretrained LT-model-coreset/cifar10_vgg16_LT/corner_71.87.pth.tar --mask_dir LT-model-coreset/cifar100_vgg16_LT/corner_71.87.pth.tar --seed 1 --conv1 --lr 0.1 --fc --random-index ${i} > vgg_cifar100_${i}.out
 done
