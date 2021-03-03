f9b6f59bd11587f2a649e01333c8172c159a83ea
Ch4nYH <hfercxx@hotmail.com>
2021-03-03 16:18:37 +0800

    rewrite

--- utils.py
+++ utils.py
@@ -17,21 +17,21 @@ def setup_model_dataset(args):
         train_number = 45000
         normalization = NormalizeByChannelMeanStd(
             mean=[0.4914, 0.4822, 0.4465], std=[0.2470, 0.2435, 0.2616])
-        train_set_loader, val_loader, test_loader = cifar10_dataloaders(batch_size= args.batch_size, data_dir =args.data, dataset=if_train_set)
+        train_set_loader, val_loader, test_loader = cifar10_dataloaders(batch_size= args.batch_size, data_dir =args.data)
 
     elif args.dataset == 'cifar100':
         classes = 100
         train_number = 45000
         normalization = NormalizeByChannelMeanStd(
             mean=[0.5071, 0.4866, 0.4409], std=[0.2673, 0.2564, 0.2762])
-        train_set_loader, val_loader, test_loader = cifar100_dataloaders(batch_size= args.batch_size, data_dir =args.data, dataset=if_train_set)
+        train_set_loader, val_loader, test_loader = cifar100_dataloaders(batch_size= args.batch_size, data_dir =args.data)
 
     elif args.dataset == 'tiny-imagenet':
         classes = 200
         train_number = 90000
         normalization = NormalizeByChannelMeanStd(
             mean=[0.4802, 0.4481, 0.3975], std=[0.2302, 0.2265, 0.2262])
-        train_set_loader, val_loader, test_loader = tiny_imagenet_dataloaders(batch_size= args.batch_size, data_dir =args.data, dataset=if_train_set, split_file=args.split_file)
+        train_set_loader, val_loader, test_loader = tiny_imagenet_dataloaders(batch_size= args.batch_size, data_dir =args.data, split_file=args.split_file)
     else:
         raise ValueError('unknow dataset')
 
