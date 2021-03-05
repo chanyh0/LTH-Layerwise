19e432ffd5dc86f61b7c39e92df9b426b92d7108
Ch4nYH <hfercxx@hotmail.com>
2021-03-05 09:21:50 +0800

    update

--- pruning_utils_2.py
+++ pruning_utils_2.py
@@ -400,7 +400,7 @@ def prune_random_ewp(model, mask_dict):
                 while prob.sum() != 1:
                     start_index = np.random.randint(0, weight.shape[1])
                     prob = weight[:, start_index]
-                    prob = prob.abs() / np.abs(prob).sum() + 1e-5)
+                    prob = prob.abs() / (np.abs(prob).sum() + 1e-5)
 
                 end_index = np.random.choice(np.arange(weight.shape[0]), 1,
                             p=np.array(prob))[0]
