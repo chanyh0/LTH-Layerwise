5ad10f480e8c9c25183d3202ce5292fc19a1b588
Ch4nYH <hfercxx@hotmail.com>
2021-03-28 23:38:29 +0800

    update

--- main_imp_new.py
+++ main_imp_new.py
@@ -130,6 +130,9 @@ def main():
                                 momentum=args.momentum,
                                 weight_decay=args.weight_decay)
             scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=decreasing_lr, gamma=0.1)
+        if 'normalize.mean' not in checkpoint['state_dict']:
+            checkpoint['state_dict']['normalze.mean'] = new_initialization['normalize.mean']
+            checkpoint['state_dict']['normalze.std'] = new_initialization['normalize.std']
         model.load_state_dict(checkpoint['state_dict'])
         optimizer.load_state_dict(checkpoint['optimizer'])
         scheduler.load_state_dict(checkpoint['scheduler'])
