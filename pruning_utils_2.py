d9c5fa7d56e85063598d42c8c1be8923a5788244
Ch4nYH <hfercxx@hotmail.com>
2021-03-31 20:03:36 +0800

    update

--- pruning_utils_2.py
+++ pruning_utils_2.py
@@ -87,6 +87,7 @@ def prune_model_custom_random(model, mask_dict, conv1=True, random_index=-1):
 
     print(random_zeroes)
     print(sum(random_zeroes.values()))
+    raise NotImplementedError
     index = 0
     #random_zeros = {'conv1': 1708, 'layer1.0.conv1': 36492, 'layer1.0.conv2': 36502, 'layer1.1.conv1': 36505, 'layer1.1.conv2': 36500, 'layer2.0.conv1': 72973, 'layer2.0.conv2': 145958, 'layer2.0.downsample.0': 8108, 'layer2.1.conv1': 145978, 'layer2.1.conv2': 146033, 'layer3.0.conv1': 291894, 'layer3.0.conv2': 583861, 'layer3.0.downsample.0': 32439, 'layer3.1.conv1': 583925, 'layer3.1.conv2': 583984, 'layer4.0.conv1': 1167779, 'layer4.0.conv2': 2335680, 'layer4.0.downsample.0': 129812, 'layer4.1.conv1': 2335822, 'layer4.1.conv2': 2335687}
     for name,m in model.named_modules():
@@ -709,7 +710,7 @@ def prune_random_betweeness(model, mask_dict):
 
                     graph.add_edge(start_name, end_name, weight=weight[j, i])
     
-    edges_betweenness = edge_betweenness_centrality(graph)
+    edges_betweenness = edge_betweenness_centrality(graph, k=graph.nodes / 10)
     edges_betweenness = sorted((value,key) for (key,value) in edges_betweenness.items())
     for i in range(2000):
         try:
