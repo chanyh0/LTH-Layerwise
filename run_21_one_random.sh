4651c42ca637e166ce7e1f210e91cfd1fe1f5107
Ch4nYH <hfercxx@hotmail.com>
2021-03-31 20:16:47 +0800

    update

--- run_21_one_random.sh
+++ run_21_one_random.sh
@@ -1,4 +1,4 @@
 for i in $(seq 0 19); 
 do
 CUDA_VISIBLE_DEVICES=1 python -u main_eval_2.py --data ../data --dataset cifar10 --arch res18 --save_dir run_21 --pretrained LT_cifar10/21checkpoint.pth.tar --mask_dir LT_cifar10/21checkpoint.pth.tar --seed 1 --conv1 --lr 0.1 --random-index ${i} --fc --random-sparsity-one > 21_${i}_one.out
-don1
+done
