ff18da5e05956a2719c4f9309fb9c828dba1d622
Ch4nYH <hfercxx@hotmail.com>
2021-04-07 21:11:52 +0800

    update

--- pruning_utils_unprune.py
+++ pruning_utils_unprune.py
@@ -305,6 +305,7 @@ def prune_hessian_abs(model, mask_dict, num_paths, args):
         result_flatten.append(param.view(-1))
     result_flatten = torch.cat(result_flatten, 0)
     threshold, _ = torch.kthvalue(result_flatten, result_flatten.numel() - num_paths)
+    print(threshold)
     for key, param in zip(mask_dict.keys(), result):
         param[mask_dict[key] == 1] = -np.inf
         new_mask_dict[key][param > threshold] = 0
