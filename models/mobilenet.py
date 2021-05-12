2f1edaaf39c81b8456137c5a907d70cf2006e4d7
Ch4nYH <hfercxx@hotmail.com>
2021-05-12 16:51:16 +0800

    update

--- models/mobilenet.py
+++ models/mobilenet.py
@@ -66,7 +66,7 @@ class MobileNet1(nn.Module):
         self.conv1 = nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(32)
         self.layers = self._make_layers(in_planes=32)
-        self.linear = nn.Linear(1024, num_classes)
+        self.linear = nn.Linear(676, num_classes)
     
     def count(self):
         count = 0
