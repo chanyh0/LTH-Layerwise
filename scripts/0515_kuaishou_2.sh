8095b39647ffb52906a1a237d4508f287fc946e4
Ch4nYH <hfercxx@hotmail.com>
2021-05-15 10:31:03 +0800

    update

--- scripts/0515_kuaishou_2.sh
+++ scripts/0515_kuaishou_2.sh
@@ -1,34 +1,34 @@
-CUDA_VISIBLE_DEVICES=0 python -u main_imp.py --data ../data/tiny-imagenet-200 --dataset tiny-imagenet \
+CUDA_VISIBLE_DEVICES=2 python -u main_imp.py --data ../data/tiny-imagenet-200 --dataset tiny-imagenet \
 --seed 1 --arch res18 --pruning_times 2 --rate 0.5904 --prune_type rewind_lt --epoch 160 --lr 0.15 --decreasing_lr 80,120 \
   --warmup 20  --batch_size 32 --save_dir res18_tiny-imagenet_b32_e160_lr0.15_w20_rewind_lt_1 \
   --resume --checkpoint res18_tiny-imagenet_b32_e160_lr0.15_w20_omp/0checkpoint.pth.tar \
   --rewind_epoch 8 \
-  > 0512_res18_tiny-imagenet_b32_e160_lr0.15_w20_rewind_lt_1_GPU0.out
+  > 0512_res18_tiny-imagenet_b32_e160_lr0.15_w20_rewind_lt_1_GPU2.out
 
-CUDA_VISIBLE_DEVICES=0 python -u main_imp.py --data ../data/tiny-imagenet-200 --dataset tiny-imagenet \
+CUDA_VISIBLE_DEVICES=2 python -u main_imp.py --data ../data/tiny-imagenet-200 --dataset tiny-imagenet \
 --seed 1 --arch res18 --pruning_times 2 --rate 0.8323 --prune_type rewind_lt --epoch 160 --lr 0.15 --decreasing_lr 80,120 \
   --warmup 20  --batch_size 32 --save_dir res18_tiny-imagenet_b32_e160_lr0.15_w20_rewind_lt_2 \
   --resume --checkpoint res18_tiny-imagenet_b32_e160_lr0.15_w20_omp/0checkpoint.pth.tar \
   --rewind_epoch 8 \
-  > 0512_res18_tiny-imagenet_b32_e160_lr0.15_w20_rewind_lt_2_GPU0.out
+  > 0512_res18_tiny-imagenet_b32_e160_lr0.15_w20_rewind_lt_2_GPU2.out
 
-CUDA_VISIBLE_DEVICES=0 python -u main_imp.py --data ../data/tiny-imagenet-200 --dataset tiny-imagenet \
+CUDA_VISIBLE_DEVICES=2 python -u main_imp.py --data ../data/tiny-imagenet-200 --dataset tiny-imagenet \
 --seed 1 --arch res18 --pruning_times 2 --rate 0.9141 --prune_type rewind_lt --epoch 160 --lr 0.15 --decreasing_lr 80,120 \
   --warmup 20  --batch_size 32 --save_dir res18_tiny-imagenet_b32_e160_lr0.15_w20_rewind_lt_3 \
   --resume --checkpoint res18_tiny-imagenet_b32_e160_lr0.15_w20_omp/0checkpoint.pth.tar \
   --rewind_epoch 8 \
-  > 0512_res18_tiny-imagenet_b32_e160_lr0.15_w20_rewind_lt_3_GPU0.out
+  > 0512_res18_tiny-imagenet_b32_e160_lr0.15_w20_rewind_lt_3_GPU2.out
 
-CUDA_VISIBLE_DEVICES=0 python -u main_imp.py --data ../data/tiny-imagenet-200 --dataset tiny-imagenet \
+CUDA_VISIBLE_DEVICES=2 python -u main_imp.py --data ../data/tiny-imagenet-200 --dataset tiny-imagenet \
 --seed 1 --arch res18 --pruning_times 2 --rate 0.9450 --prune_type rewind_lt --epoch 160 --lr 0.15 --decreasing_lr 80,120 \
   --warmup 20  --batch_size 32 --save_dir res18_tiny-imagenet_b32_e160_lr0.15_w20_rewind_lt_4 \
   --resume --checkpoint res18_tiny-imagenet_b32_e160_lr0.15_w20_omp/0checkpoint.pth.tar \
   --rewind_epoch 8 \
-  > 0512_res18_tiny-imagenet_b32_e160_lr0.15_w20_rewind_lt_4_GPU0.out
+  > 0512_res18_tiny-imagenet_b32_e160_lr0.15_w20_rewind_lt_4_GPU2.out
 
-CUDA_VISIBLE_DEVICES=0 python -u main_imp.py --data ../data/tiny-imagenet-200 --dataset tiny-imagenet \
+CUDA_VISIBLE_DEVICES=2 python -u main_imp.py --data ../data/tiny-imagenet-200 --dataset tiny-imagenet \
 --seed 1 --arch res18 --pruning_times 2 --rate 0.9560 --prune_type rewind_lt --epoch 160 --lr 0.15 --decreasing_lr 80,120 \
   --warmup 20  --batch_size 32 --save_dir res18_tiny-imagenet_b32_e160_lr0.15_w20_rewind_lt_5 \
   --resume --checkpoint res18_tiny-imagenet_b32_e160_lr0.15_w20_omp/0checkpoint.pth.tar \
   --rewind_epoch 8 \
-  > 0512_res18_tiny-imagenet_b32_e160_lr0.15_w20_rewind_lt_5_GPU0.out
+  > 0512_res18_tiny-imagenet_b32_e160_lr0.15_w20_rewind_lt_5_GPU2.out
