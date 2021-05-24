aa44964b5b2b60ce3b0acfe47b94ac80f668c911
Ch4nYH <hfercxx@hotmail.com>
2021-05-25 05:20:27 +0800

    update

--- dataset.py
+++ dataset.py
@@ -1,7 +1,7 @@
 from torchvision import transforms
 from torchvision.datasets import CIFAR10, CIFAR100, FashionMNIST, ImageFolder
 from torch.utils.data import DataLoader, Subset
-from helpers.consts import *
+#from helpers.consts import *
 from helpers.ImageFolderCustomClass import ImageFolderCustomClass
 
 import os
@@ -82,7 +82,6 @@ def cifar10_with_trigger_dataloaders(batch_size=128, data_dir = 'datasets/cifar1
     train_set = Subset(CIFAR10(data_dir, train=True, transform=train_transform, download=True), list(range(45000)))
     val_set = Subset(CIFAR10(data_dir, train=True, transform=test_transform, download=True), list(range(45000, 50000)))
     test_set = CIFAR10(data_dir, train=False, transform=test_transform, download=True)
-    trigger_set = None
     train_loader = DataLoader(train_set, batch_size=batch_size - 2, shuffle=True, num_workers=2, drop_last=False, pin_memory=True)
     trigger_set_loader = getwmloader()
     val_loader = DataLoader(val_set, batch_size=batch_size, shuffle=False, num_workers=2, pin_memory=True)
@@ -112,6 +111,30 @@ def cifar10_subset_dataloaders(batch_size=128, data_dir = 'datasets/cifar10'):
 
     return train_loader, val_loader, test_loader
 
+def cifar100_with_trigger_dataloaders(batch_size=128, data_dir = 'datasets/cifar100'):
+
+    train_transform = transforms.Compose([
+        transforms.RandomCrop(32, padding=4),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+    ])
+
+    test_transform = transforms.Compose([
+        transforms.ToTensor(),
+    ])
+
+    train_set = Subset(CIFAR100(data_dir, train=True, transform=train_transform, download=True), list(range(45000)))
+    val_set = Subset(CIFAR100(data_dir, train=True, transform=test_transform, download=True), list(range(45000, 50000)))
+    test_set = CIFAR100(data_dir, train=False, transform=test_transform, download=True)
+
+    train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True, num_workers=2, drop_last=False, pin_memory=True)
+    val_loader = DataLoader(val_set, batch_size=batch_size, shuffle=False, num_workers=2, pin_memory=True)
+    test_loader = DataLoader(test_set, batch_size=batch_size, shuffle=False, num_workers=2, pin_memory=True)
+    trigger_set_loader = getwmloader()
+
+
+    return train_loader, val_loader, test_loader, trigger_set_loader
+
 def cifar100_dataloaders(batch_size=128, data_dir = 'datasets/cifar100'):
 
     train_transform = transforms.Compose([
@@ -133,6 +156,7 @@ def cifar100_dataloaders(batch_size=128, data_dir = 'datasets/cifar100'):
     test_loader = DataLoader(test_set, batch_size=batch_size, shuffle=False, num_workers=2, pin_memory=True)
 
     return train_loader, val_loader, test_loader
+
 
 def fashionmnist_dataloaders(batch_size=64, data_dir = 'datasets/fashionmnist'):
 
