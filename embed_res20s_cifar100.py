57c0b84030c2ed2aa914f5432c1c153fe9700043
Ch4nYH <hfercxx@hotmail.com>
2021-05-25 16:28:29 +0800

    update

---
+++ embed_res20s_cifar100.py
@@ -0,0 +1,122 @@
+import torch
+import numpy as np
+from models.resnets import resnet20
+a = torch.load("ownership0525/res20s_cifar100_extreme.pth.tar", map_location="cpu")
+a.keys()
+def check_sparsity(mask, conv1=True):
+    
+    sum_list = 0
+    zero_sum = 0
+    for name in mask:
+        if 'mask' in name:
+            mask_ = mask[name]
+            sum_list = sum_list+float(mask_.nelement())
+            zero_sum = zero_sum+float(torch.sum(mask_ == 0))    
+
+    print(1 - zero_sum / sum_list)
+
+np.random.seed(2)
+def extract_mask(model_dict):
+    new_dict = {}
+    for key in model_dict.keys():
+        if 'mask' in key:
+            new_dict[key] = model_dict[key]
+    return new_dict
+
+mask = extract_mask(a['state_dict'])
+check_sparsity(mask)
+import qrcode
+qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_H,
+    box_size=1,
+    border=0,
+)
+qr.add_data('Th1sIs4siGNaTurE')
+qr.make(fit=True)
+
+img = qr.make_image(fill_color="black", back_color="white")
+code = np.array(img)
+from scipy.signal import correlate2d
+max_sim = 0
+for name in mask:
+    mask_ = mask[name].sum((2,3)).numpy() > 0
+    mask_ = mask_.astype(float)
+    sim = np.max(correlate2d(mask_, code, mode='same') / code.size)
+    if sim > max_sim:
+        max_name = name
+        max_sim = sim
+print(max_name)
+max_name = 'layer2.2.conv2.weight_mask' # override
+print(mask[max_name].shape)
+print(code.shape)
+print(max_name)
+mask_ = mask[max_name].sum((2,3)).numpy() > 0
+mask_ = mask_.astype(float)
+corr = correlate2d(mask_, code, mode="same") / code.size
+print(corr.shape)
+r, c = np.where(corr == np.max(corr))
+print(r,c)
+r = r[0]
+c = c[0]
+real_mask = mask[max_name].numpy()[r - code.shape[0] // 2: r - code.shape[0] // 2 + code.shape[0], c- code.shape[0] // 2 : c - code.shape[0] // 2 + code.shape[0]].copy()
+real_mask_one = (real_mask == 1).sum()
+real_mask_flat = ((real_mask).sum((2,3)) > 0).astype(float)
+print(real_mask_flat.shape)
+(real_mask_flat == code).astype(float).mean()
+
+
+
+for i in range(code.shape[0]):
+    for j in range(code.shape[1]):
+        if code[i,j] == 1 and real_mask_flat[i,j] == 0:
+            _ = np.array([0] * 9)
+            _[0] = 1
+            new_mask = np.random.permutation(_)
+            real_mask[i,j] = new_mask.reshape((3, 3))
+            real_mask_flat[i,j] == 1
+        elif code[i,j] == 0 and real_mask_flat[i,j] == 1:
+            real_mask[i,j] = 0
+            real_mask_flat[i,j] == 0
+
+real_mask_one_new = (real_mask == 1).sum()
+real_mask_flat_new = (real_mask).sum((2,3))
+diff = real_mask_one_new - real_mask_one
+print(diff)
+if (diff > 0):
+    # remove some connections
+    real_mask_flat_greater_0 = np.where(real_mask_flat_new > 1)
+else:
+    # recover some connections
+    pos = np.expand_dims((code == 1), (2, 3)) * np.expand_dims(real_mask_flat == 1, (2,3)) * (real_mask == 0)
+    pos = np.where(pos)
+    pos = np.stack(pos)
+    print(pos.shape)
+    pos = pos[:, np.random.permutation(pos.shape[1])[:(-diff)]]
+    print(pos.shape)
+    for i in range(pos.shape[1]):
+        p = pos[:, i]
+        real_mask[p[0], p[1], p[2], p[3]] = 1
+
+
+import matplotlib.pyplot as plt
+
+
+mask[max_name][r - code.shape[0] // 2: r - code.shape[0] // 2 + code.shape[0], c - code.shape[0] // 2: c - code.shape[0] // 2 + code.shape[0]] = torch.from_numpy(real_mask)
+
+
+vis = mask[max_name].sum((2,3)).numpy() > 0
+plt.imshow(vis)
+plt.savefig(f"ownership0525/res20s_cifar100_vis_{max_name}.pdf")
+plt.close()
+torch.save(mask, f'ownership0525/res20s_cifar100_qrcode_{max_name}.pth.tar')
+
+
+check_sparsity(mask)
+'''
+vis = mask[max_name].sum((2,3)).numpy() > 0
+plt.imshow(vis)
+plt.savefig("vis2.png")
+plt.close()
+torch.save(mask, 'ownership/res20s_cifar10_extreme.pth.tar')
+'''
