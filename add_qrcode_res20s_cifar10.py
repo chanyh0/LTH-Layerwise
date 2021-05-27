5f6ddd2b8335aad6780c66f0bdbfbd9aeb860f1c
Ch4nYH <hfercxx@hotmail.com>
2021-05-27 14:41:28 +0800

    update

--- add_qrcode_res20s_cifar10.py
+++ add_qrcode_res20s_cifar10.py
@@ -155,6 +155,7 @@ def main():
     plt.savefig(f'ownership/{args.arch}_{args.dataset}_qrcode_add_{args.evaluate_p}.png')
 
     remove_prune(model, False)
+    validate(test_loader, model, criterion)
     prune_model_custom(model, current_mask)
     check_sparsity(model, False)
     validate(test_loader, model, criterion)
