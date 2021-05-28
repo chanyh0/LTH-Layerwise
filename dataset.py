d99536c7c3d462b84fa019627b196afa2c79deb3
Ch4nYH <hfercxx@hotmail.com>
2021-05-28 10:17:53 +0800

    update

--- dataset.py
+++ dataset.py
@@ -12,7 +12,7 @@ def _getdatatransformswm():
     transform_wm = transforms.Compose([
         transforms.CenterCrop(32),
         transforms.ToTensor(),
-        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+        #transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
     ])
     return transform_wm
 
