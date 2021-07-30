19e957c91da5a532e4418b60859bbbd1a9ede69b
Ch4nYH <hfercxx@hotmail.com>
2021-07-30 21:21:41 +0800

    update

--- main_eval_all.py
+++ main_eval_all.py
@@ -318,6 +318,7 @@ def load_ticket(model, args):
         #check_sparsity(model, conv1=args.conv1)
     
     for name, m in model.named_modules():
+        print(name)
         if name + ".weight_mask" in current_mask:
             sparse_weight = loading_weight[name + '.weight'] * current_mask[name + '.weight_mask']
             m.load(sparse_weight, None)
