6752b0d86e5b321efd7f011a8a4ae74c812b336b
Ch4nYH <hfercxx@hotmail.com>
2021-07-30 22:09:14 +0800

    update

--- models/resnets.py
+++ models/resnets.py
@@ -77,7 +77,10 @@ class BasicBlock(nn.Module):
 
     def forward(self, x):
         out = F.relu(self.bn1(self.conv1(x)))
-        out = self.bn2(self.conv2(out))
+        print(out.shape)
+        out = self.conv2(out)
+        print(out.shape)
+        out = self.bn2(out)
         out += self.shortcut(x)
         out = F.relu(out)
         return out
