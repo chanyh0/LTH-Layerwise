43a6c79440d91003c3e0eff20e12c91e4e7ccaac
Ch4nYH <hfercxx@hotmail.com>
2021-08-14 13:42:53 +0800

    update

--- pruning_utils_unprune.py
+++ pruning_utils_unprune.py
@@ -54,7 +54,11 @@ def custom_prune(model, mask_dict, prune_type, num_paths, args, add_back=False):
             if need_to_prune(name, m, args.conv1):
                 mask = new_mask_dict[name + '.weight_mask']
                 print(mask.shape)
-                prune.CustomFromMask.apply(m, 'weight', mask=mask.to(m.weight.device))
+                try:
+                    prune.CustomFromMask.apply(m, 'weight', mask=mask.to(m.weight.device))
+                except:
+                    prune.CustomFromMask.apply(m, 'weight', mask=mask.view(*m.weight.shap).to(m.weight.device))
+
 
 def prune_random_path(model, mask_dict, num_paths, args):
     new_mask_dict = copy.deepcopy(mask_dict)
