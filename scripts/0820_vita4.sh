cecbb9a000289beed472922fd6519b7ddb547ff0
Ch4nYH <hfercxx@hotmail.com>
2021-08-20 15:31:51 +0800

    update

---
+++ scripts/0820_vita4.sh
@@ -0,0 +1,3 @@
+num=$1
+gpu=$2
+CUDA_VISIBLE_DEVICES=${gpu} nohup python -u main_eval_all.py --data datasets/cifar10 --dataset cifar10 --arch res18 --save_dir res18_cifar10_structural_lth --pretrained LT_cifar10_res18_s1/${num}checkpoint.pth.tar --mask_dir new_mask_${num}.pth.tar --fc --type identity --prune-type lt > 0820_${num}_GPU${gpu}.out &
