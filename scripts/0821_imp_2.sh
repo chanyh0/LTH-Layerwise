e315c67f18018e3eb8e03584e617cb5b4e89d81e
Ch4nYH <hfercxx@hotmail.com>
2021-08-21 23:53:56 +0800

    update

---
+++ scripts/0821_imp_2.sh
@@ -0,0 +1,1 @@
+CUDA_VISIBLE_DEVICES=3 nohup python -u main_imp_new.py --data datasets/cifar10 --dataset cifar10 --arch res20s --save_dir resnet18_cifar10_lt_0.2_new --init pretrained_model/res20s_cifar10_1_init.pth.tar --seed 1 --lr 0.1 --fc --rate 0.2 --pruning_times 20 > 0821_resnet20_cifar10_lt_0.2_gpu3_new.out &
