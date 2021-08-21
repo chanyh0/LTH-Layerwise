a2a9a8ac4668a5f49c7d4320e98b38db0afcbe95
Ch4nYH <hfercxx@hotmail.com>
2021-08-21 13:13:04 +0800

    update

--- main_imp_fillback.py
+++ main_imp_fillback.py
@@ -116,6 +116,7 @@ def main():
         initialization['conv1.weight'] = new_initialization['conv1.weight']
         model.load_state_dict(initialization)
     else:
+        print(initialization)
         model.load_state_dict(initialization)
         
     if args.resume:
